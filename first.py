5163d4350ab04f40c8cc5e17398a1be68b4428a0
chy <chy@qq.com>
2020-06-19 20:02:15 +0800

    定义名字类

--- first.py
+++ first.py
@@ -4,3 +4,18 @@ result = num1 + num2
 print(result)
 class peoper():
 
+
+
+
+
+
+
+
+import socket 
+import pygame 
+import time 
+class name:
+    def __init__(self):
+	self.name = name 
+	
+name('周安邦')
